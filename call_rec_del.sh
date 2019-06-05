0983c9a3c65252dbdf6d6ba238e88a9b11b10985
m-dark <i.kruk1985@gmail.com>
2019-06-05 10:10:21 +0500

    size 0

--- call_rec_del.sh
+++ call_rec_del.sh
@@ -13,7 +13,7 @@ else
 fi
 size=`df | grep $dir |  awk '{print $5}' | awk -F'%' '{print $1}'`
 sizenul=`du -sh /var/spool/asterisk/monitor | awk '{print $1}'`
-if [[ sizenul -ne 0 ]]; then
+if [[ $sizenul != 0 ]]; then
 	while [[ "$size" -ge "$limit_size" ]]; do
 		echo $date 'Диск загружен на: '$size'%' >> $logfile
 		cd $dir_del
