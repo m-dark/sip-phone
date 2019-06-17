a5a86c05345dc460307bd286cbe04356042ba447
m-dark <i.kruk1985@gmail.com>
2019-06-17 10:56:51 +0500

    call_rec_del.sh

--- call_rec_del.sh
+++ call_rec_del.sh
@@ -6,28 +6,30 @@ date=`date +%Y.%m.%d' '%H:%M:%S`
 #limit_size=70								#Лимит занятого места в процентах, после которого удаляются файлы.
 dir=$1
 limit_size=$2
-if [[ $dir=dev/mapper/SangomaVG-root ]]; then
+if [[ "$dir" = dev/mapper/SangomaVG-root ]]; then
 	dir_del=/var/spool/asterisk/monitor
 else
 	dir_del=$dir
 fi
 size=`df | grep $dir |  awk '{print $5}' | awk -F'%' '{print $1}'`
-sizenul=`du -sh $dir_del | awk '{print $1}'`
-if [[ $sizenul != 0 ]]; then
-	while [[ "$size" -ge "$limit_size" ]]; do
-		echo $date 'Диск загружен на: '$size'%' >> $logfile
-		cd $dir_del
-		dir_year=`(ls -lr | tail -n 1) | awk -F' ' '{print $9}'`	#Находим самый старый каталог (сортировка по названию файла) год
-		cd $dir_del/$dir_year
-		dir_month=`(ls -lr | tail -n 1) | awk -F' ' '{print $9}'`	#Находим самый старый каталог (сортировка по названию файла) месяц
-		cd $dir_del/$dir_year/$dir_month
-		dir_day=`(ls -lr | tail -n 1) | awk -F' ' '{print $9}'`		#Находим самый старый каталог (сортировка по названию файла) день
-		file_del=`ls -l $dir_del/$dir_year/$dir_month/$dir_day`
-			echo $date 'Удалили каталог '$dir_del/$dir_year/$dir_month/$dir_day >> $logfile
-			echo $date 'C файлами ' $file_del >> $logfile
-		rm -rf $dir_del/$dir_year/$dir_month/$dir_day			#Удаляем каталог со всеми файлами. один день записей.
-		size=`df -l | grep $dir |  awk '{print $5}' | awk -F'%' '{print $1}'`
-		date=`date +%Y.%m.%d' '%H:%M:%S`
-	done
-fi
+sizenul=`du -shm $dir_del | awk '{print $1}'`
+echo $size
+echo $dir_del
+echo $sizenul
+while [[ "$size" -ge "$limit_size" && "$sizenul" -gt 2000 ]]; do
+	echo $date 'Диск загружен на: '$size'%' >> $logfile
+	cd $dir_del
+	dir_year=`(ls -lr | tail -n 1) | awk -F' ' '{print $9}'`	#Находим самый старый каталог (сортировка по названию файла) год
+	cd $dir_del/$dir_year
+	dir_month=`(ls -lr | tail -n 1) | awk -F' ' '{print $9}'`	#Находим самый старый каталог (сортировка по названию файла) месяц
+	cd $dir_del/$dir_year/$dir_month
+	dir_day=`(ls -lr | tail -n 1) | awk -F' ' '{print $9}'`		#Находим самый старый каталог (сортировка по названию файла) день
+	file_del=`ls -l $dir_del/$dir_year/$dir_month/$dir_day`
+		echo $date 'Удалили каталог '$dir_del/$dir_year/$dir_month/$dir_day >> $logfile
+		echo $date 'C файлами ' $file_del >> $logfile
+	rm -rf $dir_del/$dir_year/$dir_month/$dir_day			#Удаляем каталог со всеми файлами. один день записей.
+	size=`df -l | grep $dir |  awk '{print $5}' | awk -F'%' '{print $1}'`
+	date=`date +%Y.%m.%d' '%H:%M:%S`
+	sizenul=`du -shm $dir_del | awk '{print $1}'`
+done
 exit 0
