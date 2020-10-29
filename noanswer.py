95213a53e3467991e067cc722855ce58b04948f2
m-dark <kruk.ivan@itmh.ru>
2020-10-29 14:08:35 +0500

    new fwconsole reload

--- noanswer.py
+++ noanswer.py
@@ -295,6 +295,6 @@ db.close()
 
 if restart==1:
 	subprocess.call("sudo /usr/bin/alias runuser=/usr/sbin/runuser", shell=True)
-	subprocess.call("sudo /var/lib/asterisk/bin/retrieve_conf", shell=True)
+	subprocess.call("sudo /var/lib/asterisk/bin/fwconsole reload", shell=True)
 #	subprocess.call("fwconsole reload", shell=True)
 	subprocess.call("sudo /var/lib/asterisk/bin/module_admin reload", shell=True)
